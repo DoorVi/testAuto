f2b6fdb4fc9f0bd3aebab982c6d7595e9666aac9
dorvi <1249468759@qq.com>
2022-09-21 11:38:24 +0800

    first commit

---
+++ src/main/java/com/testauto/testauto/Controller/Test.java
@@ -0,0 +1,19 @@
+package com.testauto.testauto.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: dorvi
+ * @Date: 2022/9/21
+ * @Theme:
+ **/
+@RestController
+public class Test {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
